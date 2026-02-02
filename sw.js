dd114dda911518e344a010ba961a60de89c89c97
Shivangi Prasad <shivangi.kgp@gmail.com>
2026-02-02 09:57:04 -0600

    Fix PWA install issues

--- sw.js
+++ sw.js
@@ -18,6 +18,20 @@ self.addEventListener('install', (event) => {
   );
 });
 
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
